936e4b04a95c220c0bf310e499813340546bbfd3
jahidstm <jahidhasanstm@gmail.com>
2025-10-08 21:58:12 +0600

    python

---
+++ Simple Code/conditional_statements.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("This is a positive number.")
+elif number == 0:
+    print("This is zero.")
+else:
+    print("This is a negative number.")
